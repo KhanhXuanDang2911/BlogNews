bc74e822b70a0f108d20f7b1b40c4283d0c7ee01
KhanhXuanDang2911 <caubesuuca321@gmail.com>
2024-12-07 20:20:44 +0700

    Update model

--- src/main/java/model/dao/DBContext.java
+++ src/main/java/model/dao/DBContext.java
@@ -1,4 +1,20 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBContext {
+    protected Connection connection;
+    public DBContext(){
+        final String URL = "jdbc:mysql://localhost:3306/blognews";
+        final String USERNAME = "root";
+        final String PASSWORD = "291104";
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        }catch(ClassNotFoundException | SQLException e){
+            System.out.println(e);
+        }
+    }
 }
